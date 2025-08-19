a2d727a94e957a13ffc52e0142df50dc3f871f73
pknowledge <jamesblssings@gmail.com>
2025-08-19 21:44:06 +0300

    sql

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+("Mr. Chocolate", 20, "M", "brown", "calm", 1),
+("Rowdy", 10, "M", "black", "intense", 1),
+("Tabitha", 6, "F", "white", "friendly", 1),
+("Sergeant Brown", 12, "M", "brown", "serious", 1),
+("Melissa", 8, "F", "gray", "gentle", 1),
+("Grinch", 15, "M", "green", "grumpy", 0),
+("Wendy", 7, "F", "brown", "playful", 1),
+(NULL, 3, "M", "black", "mysterious", 1);
